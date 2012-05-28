eef77122a718c4782f6771ac0a258ff4355686c5
Bjorn Wijers <burobjorn@burobjorn.nl>
2012-05-28 14:39:28 +0200

    initial commit

---
+++ readme.txt
@@ -0,0 +1,21 @@
+=== Redirect to Homepage ===
+Contributors: BjornW
+Tags: redirect, login, simple
+Requires at least: 2.7
+Tested up to: 3.3.2 
+Stable tag:trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Redirects a logged in user to the homepage instead of the Dashboard.
+
+== Description ==
+After a user has logged in, the user will be redirected to the homepage
+instead of the Dashboard when this plugin is activated. The plugin has no
+settings and should work perfectly fine with Multisite installations
+(the plugin will use the blog url and _not_ the main site url).
+
+== Installation ==
+1. Upload the `redirect2homepage` directory to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Done!
---
+++ redirect2homepage.php
@@ -0,0 +1,40 @@
+<?php
+/******************************************************************************
+Plugin Name: Redirect To Homepage
+Plugin URI: http://burobjorn.nl
+Description: Redirect a logged in user to the homepage after the user logs in.
+Version: 1.0
+Author: Bjorn Wijers <burobjorn at burobjorn dot nl>
+Author URI: http://burobjorn.nl
+License: GPLv2 or later
+******************************************************************************/
+
+/*  Copyright 2012
+RedirectToHomepage is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+RedirectToHomepage is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/**
+ * Uses the login_redirect filter which apperently exists since WordPress 2.7
+ * according to http://adambrown.info/p/wp_hooks/hook/login_redirect
+ * thus this should work from WordPress 2.7 and upwards.
+ */
+if( ! function_exists('redirectt2homepage') ) {
+  function redirect2homepage()
+  {
+    return get_bloginfo('url');
+  }
+add_filter('login_redirect', 'redirect2homepage');
+}
+?>
